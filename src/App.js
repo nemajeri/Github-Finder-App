c907e1e4460b0b68bbb135ed31e203c7c7991a85
nemanja19999 <94128039+nemanja19999@users.noreply.github.com>
2022-01-18 22:27:02 +0100

    Added Home and Not Found Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
-import Users from './components/Users/Users';
 import User from './components/Users/User';
 import Githubstate from './context/github/GithubState';
 import AlertState from "./context/alert/AlertState";
 import './App.css';
-import Search from './components/Users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
 
@@ -22,16 +22,10 @@ const App = () => {
         <div className='container'>
           < Alert alert={alert}/>
           <Switch>
-            <Route exact path='/' 
-            render={props => (
-              <Fragment>
-               <Search />
-               <Users />
-              </Fragment>
-               )}
-           />
+            <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About} />
           <Route exact path='/user/:login' component={User}/> 
+          <Route component={NotFound}/>
           </Switch>
         
         </div> 
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,19 @@ import {
     GET_REPOS
 } from '../types'
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
+} else {
+     githubClientId = process.env.GITHUB_CLIENT_ID;
+     githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+}
+
 
 const Githubstate = props => {
     const initialState = {
@@ -27,8 +40,8 @@ const searchUsers = async text => {
   setLoading()
 
   const res = await axios.get(`https://api.github.com/search/users?q=${text}
-  &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  &client_id=${githubClientId}
+  &client_secret=${githubClientSecret}`);
 
   dispatch({
     type: SEARCH_USERS,
@@ -42,8 +55,7 @@ const getUser = async (username) => {
   setLoading()
 
   const res = await axios.get(`https://api.github.com/users/${username}?client_id=
-  ${process.env.REACT_APP_GITHUB_CLIENT_ID
-  }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  ${githubClientId }&client_secret=${githubClientSecret}`
   );
 
   dispatch({
@@ -57,8 +69,7 @@ const getUserRepos = async username => {
   setLoading()
 
   const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
-  ${process.env.REACT_APP_GITHUB_CLIENT_ID
-  }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  ${githubClientId}&client_secret=${githubClientSecret}`
   );
 
   dispatch({
